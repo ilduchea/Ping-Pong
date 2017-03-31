af84b8eaa7de376f227d59291f5808b22ac88dfc
ilduchea <ilduchea@gmail.com>
2017-03-31 09:30:13 -0700

    add js to gather the users inputted number on clicking of the submit button. Add function with logic to check that the number entered is a whole number otherwise round the number and let the user know that the number has been rounded.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,23 @@
+var pingPong = function(userInput) {
+  var userNumber = parseFloat(userInput);
+  var pingPongNumber = roundNuber(userNumber);
+  console.log(pingPongNumber);
+};
+
+var roundNuber = function (number) {
+  if (number % 1 === 0){
+    return number;
+  } else {
+    $('.error').text("Your number has been rounded to the nearest whole number.")
+    return Math.round(number);
+  }
+};
 $(function(){
+  $('#submit').click(function() {
+    var userInput = $('.user-input input[name=number]').val();
+    var result = pingPong(userInput);
+
+  });
 
 
-  
 });
